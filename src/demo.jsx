28fac3771faa93cdbde98ef6ef3fa473337481ba
unknown <zhanglei@smartpay.com>
2018-12-10 11:58:52 +0800

    @

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from './react-redux/index';
 
-// @connect
+@connect
 class Demo extends Component {
     constructor(props) {
         super(props);
